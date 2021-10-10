a8ab23757e776d56fc7041aac88dde0bfa0519d1
prateik1123 <70634930+prateik1123@users.noreply.github.com>
2021-10-10 20:07:26 +0530

    Update index.js

--- examples/index.js
+++ examples/index.js
@@ -3,6 +3,6 @@ const { getQuote, getRandomQuote } = require('../lib/index'),
 
 console.log(`Here's a quote by "${quote.author}": "${quote.text}"\n`); // will return quote with author
 
-console.log(getRandomQuote()); // will return a random quote
+console.log(getRandomQuote()); // will return a random quote , do this
 
-console.log(getQuote({ author: false })); // will return quote without author
+console.log(getQuote({ author: false })); // will return quote without author, dont do this
